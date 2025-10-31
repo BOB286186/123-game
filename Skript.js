783728e45ea22cb2890224ef0b001eed7cfe6830
BOB286186 <u0678456331@gmail.com>
2025-11-01 00:57:38 +0100

    Implement number guessing game Logik

---
+++ Skript.js
@@ -0,0 +1,23 @@
+let NumberToGues = Math.round(Math.random() *100);
+let tries = 0;
+function guessTheNumber() {
+    tries = tries + 1;
+    displayTries.innerHTML = 'Versuche: ' + tries;
+if(NumberToGues == myNumber.value) {
+        headline.innerHTML = 'Du hast gewonnen!!! 🥳🎉';
+        let jsConfetti = new JSConfetti();
+        jsConfetti.addConfetti()
+        jsConfetti.addConfetti({
+        emojis: ['❄', '🥳', '❄', '🥳', '❄', '🥳'],});
+    }
+
+
+if(NumberToGues > myNumber.value) {
+        headline.innerHTML = 'Deine zahl ist zu Klein';
+
+}
+if(NumberToGues < myNumber.value) {
+        headline.innerHTML = 'Deine zahl ist zu Groß';
+}
+myNumber.value ='';
+}
